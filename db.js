4dce2f89ce4c2f6c93322f53546aef73759b4a09
Vinay <vinaykgupta04@gmail.com>
2025-05-07 15:58:49 +0530

    Change in DB3

--- db.js
+++ db.js
@@ -4,8 +4,10 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try{
-
+        mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB connected successfully');
     }catch(error){
-
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1);
     }
 };
